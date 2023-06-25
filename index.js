05318238260df377ef6922e9aa814a828d477eca
A V Kishore Kumar <75232316+kishorekumar14@users.noreply.github.com>
2023-06-25 12:49:31 +0530

    Update date in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const opendate = '12 Aug 2022';
+const opendate = '30 Aug 2024';
 
 function countdown() {
     const currentDate = new Date();
